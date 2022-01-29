5ae666c4abd0d0411560f56b58d4241f14e4bb0e
Akash Mathwani <2015eeb1045@iitrpr.ac.in>
2022-01-29 13:47:12 +0530

    added apollo server start

---
+++ src/apollo-server.js
@@ -0,0 +1,40 @@
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import express from "express";
+import http from "http";
+import dotenv from "dotenv";
+dotenv.config({ path: `./${process.env.NODE_ENV}.env` });
+
+const PORT = process.env.PORT || 4000;
+
+async function startApolloServer() {
+  // Required logic for integrating with Express
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  // Same ApolloServer initialization as before, plus the drain plugin.
+  const server = new ApolloServer({
+    typeDefs: [],
+    resolvers: [],
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  // More required logic for integrating with Express
+  await server.start();
+  server.applyMiddleware({
+    app,
+
+    // By default, apollo-server hosts its GraphQL endpoint at the
+    // server root. However, *other* Apollo Server packages host it at
+    // /graphql. Optionally provide this to match apollo-server.
+    path: "/",
+  });
+
+  // Modified server startup
+  httpServer.listen({ port: PORT });
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
+}
+
+export default startApolloServer;
